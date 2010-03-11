0d3612e6677f042cb8aa222929f6371e8776c0e3
rngtng <tobi@rngntg.com>
2010-03-11 23:37:46 +0100

    added master<->slave register communitcation (not yet working) :-(

--- firmware/Connection.cpp
+++ firmware/Connection.cpp
@@ -11,28 +11,43 @@
 
 Connection Con; 
 
-void receive_register_response_callback(int howMany) {
-  if( Wire.receive() == I2C_CMD_OK) {  //TODO send explizit -set address- command
+void onReceiveInitCallback(int howMany) {
+  uint8_t command = Wire.receive();  
+  if( command == I2C_CMD_OK) {  //Maybe TODO send explizit -I2C_CMD_SET_ADDRESS- command
     Con.i2c_address = Wire.receive();
   }
 }
 
-void receive_register_request_callback(int howMany) {
-  if(Wire.receive() == I2C_CMD_HELLO) {
-    //Con.register_slave( Wire.receive() );
+//Master I2C Callback
+void onReceiveMasterCallback(int howMany) {
+  uint8_t command = Wire.receive();
+  if( command == I2C_CMD_HELLO) {
+    //if( Con.slave_address_to_register == 0 ) 
+      Con.slave_address_to_register = Wire.receive(); //we can't instant replay here ;-(
   }
-  //else pass data to serial???
+  else {   //forward data to serial
+    Serial.write(command); //TODO Serial within Wire is bad!???
+    //TODO send slave number???
+    //TODO send message length??
+    while( howMany > 1 ) {
+      Serial.write( Wire.receive() );
+      howMany--;
+    }
+  }
 }
 
-void receive_command_callback(int howMany) {
-  //Con.processMessage(Wire.receive() );
-  //TODO send recieved byte to Con.
+//Slave I2C Callback
+void onReceiveSlaveCallback(int howMany) {
+  Con.inputBufferLength = howMany; //MaybeTODO use explizit -initMessage- command
+  while( howMany > 0 ) {
+    Con.processMessage( Wire.receive() );
+    howMany--;
+  }
 }
 
-void Connection::beginAuto() { //TODO I2C & Serail callback FNC ??
+void Connection::begin() {
   master = false;
   i2c_address = 0;
-  last_slave_address = 0;
   //TODO serial = _serial;
   //TODO baud_rate = _baud_rate;
 
@@ -40,45 +55,50 @@ void Connection::beginAuto() { //TODO I2C & Serail callback FNC ??
   i2c_address = EEPROM.read(I2C_EEPROM_ADR) & 0x0F;
 
   // 2. startup delay 
-  int delay_time = i2c_address * STARTUP_DELAY_FACTOR;
+  int delay_time =  i2c_address * STARTUP_DELAY_FACTOR;
   delay(delay_time);
-
+  
   // 3. open I2C connection with I2C_START_ADR as offset
-  i2c_address += I2C_START_ADR;
+  i2c_address += I2C_START_ADR;  
   Wire.begin(i2c_address);
 
   // 4. register Callback fnc
-  Wire.onReceive(receive_register_response_callback);
+  Wire.onReceive(onReceiveInitCallback);
 
   //5. wait for response with new slave address
-  int c = I2C_WAIT_FOR_ADDRESS_RETRYS;
+  uint8_t c = I2C_WAIT_FOR_ADDRESS_RETRYS;  
   while( i2c_address >= I2C_START_ADR && c > 0) {  
     // 5a. make call to master with static number.
     Wire.beginTransmission(I2C_MASTER_ADR);
     Wire.send(I2C_CMD_HELLO);
     Wire.send(i2c_address);   //send adress where to respond to
     Wire.endTransmission();
-
     delay(I2C_WAIT_FOR_ADDRESS_TIMEOUT);
     c--;
   }  
 
   if(i2c_address < I2C_START_ADR) {  //success
-    beginSlave(i2c_address);
+    beginSlave(i2c_address, false);
   }
   else { // failure
-    beginMaster();
+    beginMaster(I2C_MASTER_ADR, false);
   }
 }
 
-void Connection::beginMaster(int master_address) {
+void Connection::beginMaster(uint8_t master_address, bool update_adress) {
   //6b. use master I2C address, start master mode, open serial
   i2c_address = master_address;
   Serial.begin(BAUD_RATE);
 
   //7. start new I2C with given address
-  Wire.begin(i2c_address);
-  Wire.onReceive(receive_register_request_callback);
+  if(update_adress) {
+    Wire.begin(i2c_address);
+  }
+  else {
+    TWAR = i2c_address << 1; // twi_setAddress
+  }
+   
+  Wire.onReceive(onReceiveMasterCallback);
   master = true;
 
   //8. save new adress to EEPROM
@@ -86,28 +106,59 @@ void Connection::beginMaster(int master_address) {
   
   messageAwaiting = false;
   inputBufferLength = 0;
+  slave_address_to_register = 0;
   outputBuffer = &buffer1[0];
   inputBuffer  = &buffer2[0];
+  
+  last_slave_address = I2C_SLAVE_ADR;
 }
 
-void Connection::beginSlave(int slave_address) {
+void Connection::beginSlave(uint8_t slave_address, bool update_adress) {
   //6a. use slave I2C adress, start slave mode
   i2c_address = slave_address;
 
   //7. start new I2C with given address
-  Wire.begin(i2c_address);   
-  Wire.onReceive(receive_command_callback);
+  if(update_adress) {
+    Wire.begin(i2c_address);
+  }
+  else {
+    TWAR = i2c_address << 1; // twi_setAddress
+  }  
+  Wire.onReceive(onReceiveSlaveCallback);
   master = false;
 
   //8. save new adress to EEPROM
   EEPROM.write(I2C_EEPROM_ADR, i2c_address);
 }
 
+/******************************************************************/
+void Connection::registerPendingSlave() {
+  if( slave_address_to_register == 0) return;
+  delay(1000);
+  last_slave_address++;
+  Wire.beginTransmission(8); //slave_address_to_register);
+  Wire.send(I2C_CMD_OK);
+  Wire.send(last_slave_address);   //send adress where to respond to
+  Wire.endTransmission();
+
+  //TODO notify serial about new slave
+
+  slave_address_to_register = 0;
+}
+
+
+/******************************************************************/
 void Connection::onMessageAvailable(conCallbackFunction newFunction) {
   callback = newFunction;
 }
 
-uint8_t Connection::process(byte serialByte) {
+void Connection::loop() {
+  if( Serial.available() ) process( Serial.read() );
+  registerPendingSlave();
+}  
+
+
+uint8_t Connection::process(uint8_t serialByte) {
   //DEBUG Rainbowduino.set_frame_line(0,1,serialByte,0,0);
   if( !messageAwaiting && serialByte == MESSAGE_START ) {
     messageAwaiting = true;
@@ -123,7 +174,7 @@ uint8_t Connection::process(byte serialByte) {
   processMessage(serialByte);
 }
 
-uint8_t Connection::processMessage(byte serialByte) {
+uint8_t Connection::processMessage(uint8_t serialByte) {
   if( !messageAwaiting || inputBufferLength < 1 ) return 3;
   inputBuffer[inputBufferIndex] = serialByte;
   inputBufferIndex++;
@@ -157,12 +208,17 @@ uint8_t Connection::available() {
 }
 
 
-void Connection::write(int w) {
+void Connection::write(uint8_t w) {
 }
 
-void Connection::ok(byte command, byte param) {
-  Serial.flush();
-  Serial.write(MESSAGE_OK);
-  Serial.write(command);
-  Serial.write(param);
+void Connection::ok(uint8_t command, uint8_t param) {
+  if( master ) {
+    Serial.flush();
+    Serial.write(MESSAGE_OK);
+    Serial.write(command);
+    Serial.write(param);
+  }
+  else {
+    //TODO I2C
+  }
 }
--- firmware/Connection.h
+++ firmware/Connection.h
@@ -14,7 +14,7 @@
 
 #define BAUD_RATE 57600
 
-#define I2C_START_ADR 100
+#define I2C_START_ADR 64
 #define I2C_MASTER_ADR 7
 #define I2C_SLAVE_ADR 10
 
@@ -28,13 +28,13 @@
 
 #define STARTUP_DELAY_FACTOR 100 //in ms
 
-#define MESSAGE_BUFFER_SIZE 64 //max 128
+#define MESSAGE_BUFFER_SIZE 32 //max 128
 #define MESSAGE_START 255
 #define MESSAGE_OK 1
 
 #include <Wire.h>
 #include "WProgram.h"
-//#include "Rainbowduino.h"
+//DEBUG #include "Rainbowduino.h"
 
  extern "C" {
  // callback function
@@ -45,38 +45,43 @@ class Connection {
 
 public:
   bool master;
-  volatile int i2c_address;
-  volatile int last_slave_address;
-  //volatile int last_slave_address = 0; //slave number to adress mapper ??
+  volatile uint8_t i2c_address;
+  volatile uint8_t last_slave_address;
+  volatile uint8_t slave_address_to_register;
   
-  void beginAuto();
-  void beginMaster(int master_address = I2C_MASTER_ADR);
-  void beginSlave(int slave_address);
+  void loop();
+  void begin();
+  void beginMaster(uint8_t master_address = I2C_MASTER_ADR, bool update_adress = true);
+  void beginSlave(uint8_t slave_address, bool update_adress = true);
   void onMessageAvailable(conCallbackFunction newFunction);
   int read();
-  uint8_t process(byte serialByte);
-  uint8_t processMessage(byte serialByte);
+  uint8_t process(uint8_t serialByte);
+  uint8_t processMessage(uint8_t serialByte);
   uint8_t available();
   
-  void ok(byte command, byte param);
+  void registerPendingSlave();
+  
+  void ok(uint8_t command, uint8_t param);
   
   //TODO
-  void write(int data);
+  void write(uint8_t data);
+  
+  uint8_t inputBufferLength; //number of bytes to receive
 
 private: 
   bool messageAwaiting; //status wheter Message parsing is in progress
   
   conCallbackFunction callback;
   
-  byte buffer1[MESSAGE_BUFFER_SIZE]; // Buffer that holds the data
+  uint8_t buffer1[MESSAGE_BUFFER_SIZE]; // Buffer that holds the data
   byte* inputBuffer; // Pointer to current data
-  uint8_t inputBufferLength; //number of bytes to receive
+
   uint8_t inputBufferIndex; // Index where to write the data
   
-  byte buffer2[MESSAGE_BUFFER_SIZE]; // Buffer that holds the data
+  uint8_t buffer2[MESSAGE_BUFFER_SIZE]; // Buffer that holds the data
   byte* outputBuffer;
   uint8_t outputBufferLength;
-  uint8_t outputBufferIndex; // Index where to write the data
+  uint8_t outputBufferIndex; // Index where to write the data  
 };
 
 extern Connection Con;
