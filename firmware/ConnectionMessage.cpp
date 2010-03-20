9e144919d027b8d7ae38389ef9148c305009670d
rngtng <tobi@rngntg.com>
2010-03-20 14:22:27 +0100

    split Connection and Message into two seperate Files, implemented forwarding Message to client and response from client to Master

---
+++ firmware/ConnectionMessage.cpp
@@ -0,0 +1,71 @@
+/*
+* Implementation
+*/
+
+#include "ConnectionMessage.h"
+#include "WProgram.h"
+
+ConnectionMessage::ConnectionMessage() {
+  reset();
+}
+
+void ConnectionMessage::consume( uint8_t dataByte ){
+	if(ready()) return;
+	if( writeIndex == 0 && dataByte != COMMAND && dataByte != OK && dataByte != ERROR) return;
+  data[writeIndex++] = dataByte;
+}
+
+void ConnectionMessage::reset() {
+  writeIndex = 0;
+  readIndex = 0;
+}
+
+bool ConnectionMessage::ready() {
+return (writeIndex == totalLength());
+}
+
+bool ConnectionMessage::isError() {
+  return type() == ERROR;
+}
+
+bool ConnectionMessage::isOk() {
+  return type() == OK;
+}
+
+bool ConnectionMessage::isCommand() {
+  return type() == COMMAND;
+}
+
+bool ConnectionMessage::is(uint8_t command) {
+  return ready() && data[INDEX_COMMAND] == command;
+}
+
+uint8_t ConnectionMessage::type() {
+  if( !ready() ) return 0;  
+  return data[INDEX_TYPE];
+}
+
+uint8_t ConnectionMessage::receiver() {
+  if( !ready() ) return 0;  
+  return data[INDEX_RECEIVER];
+}
+
+uint8_t ConnectionMessage::command() {
+  if( !ready() ) return 0;  
+  return data[INDEX_COMMAND];
+}
+
+uint8_t ConnectionMessage::param() {
+  if( !ready() ) return 0;  
+  return data[HEADER_LENGTH];
+}
+
+uint8_t ConnectionMessage::paramRead() {
+  if( !ready() ) return 0;
+  return data[HEADER_LENGTH + readIndex++];
+}
+
+uint8_t ConnectionMessage::totalLength() {
+ if(writeIndex >= HEADER_LENGTH) return HEADER_LENGTH + data[INDEX_LENGTH];
+ return HEADER_LENGTH;
+}
---
+++ firmware/ConnectionMessage.h
@@ -0,0 +1,51 @@
+/* Com:
+ * Abstraction of Serial / Wire commmunication
+ * message :
+ * 1. type: OK | COMMAND | ERROR
+ * 2. receiver
+ * 3. command
+ * 4. data length
+ * 5. data
+ */
+ 
+#ifndef ConnectionMessage_h
+#define ConnectionMessage_h
+
+#define MESSAGE_DATA_SIZE 32 //max 128
+#define HEADER_LENGTH 4
+
+/* Header Codes */
+#define COMMAND     0xFF  // followed by commands
+#define OK          0x0F  // followed by return params
+#define ERROR       0x01  // followed by error code
+
+#define INDEX_TYPE     0
+#define INDEX_RECEIVER 1
+#define INDEX_COMMAND  2
+#define INDEX_LENGTH   3
+
+#include "WProgram.h"
+
+class ConnectionMessage {
+ public:
+   uint8_t data[MESSAGE_DATA_SIZE];
+   uint8_t writeIndex;
+   uint8_t readIndex;
+   
+   ConnectionMessage();
+   void consume( uint8_t dataByte );
+   void reset();
+   bool ready();
+   bool isError();
+   bool isOk();
+   bool isCommand();
+   bool is(uint8_t command);
+   uint8_t type();
+   uint8_t receiver();
+   uint8_t command();
+   uint8_t param();
+   uint8_t paramRead();
+   uint8_t totalLength();
+};
+
+#endif
